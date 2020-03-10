dec4a65f0ece0f487d7d24677c405f335fc73cc4
drsg85 <drsg85@gmail.com>
2020-03-10 21:50:58 +0200

    Add slider functionality to aboutus section and basic styles

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,32 @@
 'use strict';
 import MobileMenu from './modules/MobileMenu';
+import { tns } from '../../node_modules/tiny-slider/src/tiny-slider.js';
 // import ToTop from './modules/toTop';
 
 const mobileMenu = new MobileMenu();
+
+
 // new ToTop();
+
+if(document.querySelector('.aboutus__staff')) {
+    const slider = new tns({
+        container: '.my-slider',
+        mode: "carousel",
+        items: 1,
+        fixedHeight: 250,
+        controls: true,
+        nav: false,
+        controlsContainer: '.my-slider__nav',
+        responsive: {
+          640: {
+            items: 2
+          },
+          700: {
+          },
+          900: {
+            fixedWidth: 265,
+            items: 4
+          }
+        }
+      });
+}
