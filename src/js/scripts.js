084eab6a0bd3121cc9e8c398429892d4a434905b
drsg85 <drsg85@gmail.com>
2020-04-04 16:50:18 +0200

    Finish styles for clients section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -34,3 +34,31 @@ if(document.querySelector('.aboutus__staff')) {
         }
       });
 }
+
+if(document.querySelector('.clients__slider')) {
+    const slider = new tns({
+        container: '.clients-slider',
+        mode: "carousel",
+        items: 1,
+        // autoplay: true,
+        nav: true,
+        navContainer: '.clients__dots',
+        controls: false,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        speed: 350,
+        navPosition: 'bottom',
+        mouseDrag: true,
+        responsive: {
+          640: {
+            // items: 2,
+            // gutter: 25
+          },
+          700: {
+          },
+          900: {
+            items: 1
+          }
+        }
+      });
+}
